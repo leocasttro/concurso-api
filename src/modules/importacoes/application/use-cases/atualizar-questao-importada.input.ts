bb328579997182e2ae08de39dab19377d6f0e881
leocasttro <leocasttro01@icloud.com>
2026-08-09 11:52:14 -0300

    fix(gabarito) correcao pendente alinhando gabarito ao contrato estabelecido

--- src/modules/importacoes/application/use-cases/atualizar-questao-importada.input.ts
+++ src/modules/importacoes/application/use-cases/atualizar-questao-importada.input.ts
@@ -1,4 +1,5 @@
 import { TipoQuestaoValor } from '../../../questoes/domain/value-objects/tipo-questao.vo';
+import { GabaritoImportado } from '../../domain/entities/questao-importada.entity';
 
 export type AtualizarQuestaoImportadaInput = {
   importacaoId: string;
@@ -9,10 +10,7 @@ export type AtualizarQuestaoImportadaInput = {
     texto: string;
     letra?: string;
   }>;
-  gabarito?: {
-    tipo: string;
-    valores: string[];
-  };
+  gabarito?: GabaritoImportado;
   disciplina?: string;
   assunto?: string;
   textoApoio?: string;
